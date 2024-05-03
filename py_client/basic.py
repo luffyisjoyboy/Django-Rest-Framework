3b6457c7644879cd91893c0a15edb3b04b9c0dd2
Akhilesh naredla <anaredla@Luffy.grandenetworks.net>
2024-05-03 01:02:12 -0500

    post method testing through serializer
    we need to create an instance if we are creating custom methods in serializers.py script held write handlers and tested raise exception in serializer.is_valid

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,23 +1,5 @@
 import requests
 
-# endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
-
-# When sending json in parameters output would be in data key and content type is application/json
-response = requests.get(endpoint, params={"abc": 123}, json={"query":"Hello world!"})
-
-# prints json: raw text response
-# print(response.text)
-# print(response.status_code)
+response = requests.post(endpoint, params={"abc": 123}, json={"content":"Hello world!"})
 print(response.text)
-
-# When sending json in parameters output would be in form key and content type is application/x-www-form-urlencoded
-# response = requests.get(endpoint, data={"query":"Hello world!"})
-
-
-# HTTP Request -> HTML
-# REST API HTTP Request -> json
-# Javascript object Notation
-# Typically what you want to send is what you expect it to be recieved like sending a json you would be expecting a json as well mostly
-# converts json to dict
-# print(response.json())
